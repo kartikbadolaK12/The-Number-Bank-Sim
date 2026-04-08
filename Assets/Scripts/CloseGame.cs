b7230c0aec175bf50a0911aa3c154896bd3a6fa9
kartikbadola <kartik.badola@k12onlineschools.com>
2026-04-08 10:44:43 +0530

    Second

--- Assets/Scripts/CloseGame.cs
+++ Assets/Scripts/CloseGame.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class CloseGame : MonoBehaviour
 {
-    // Import JavaScript function (WebGL)
-    [DllImport("__Internal")]
-    private static extern void CloseWindow();
+//     // Import JavaScript function (WebGL)
+//     [DllImport("__Internal")]
+//     private static extern void CloseWindow();
 
-    public void QuitGame()
-    {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        CloseWindow();     // WebGL - Close browser tab (if allowed)
-#else
-        Application.Quit(); // PC / Android / iOS
-#endif
-    }
+//     public void QuitGame()
+//     {
+// #if UNITY_WEBGL && !UNITY_EDITOR
+//         // CloseWindow();     // WebGL - Close browser tab (if allowed)
+// #else
+//         Application.Quit(); // PC / Android / iOS
+// #endif
+//     }
 }
